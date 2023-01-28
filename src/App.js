8e80ac3770055b8ea6e90f146e09a9f6597d8b47
Jose Ramos <engrjvramos@gmail.com>
2023-01-28 19:59:32 +0800

    added scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -16,36 +16,42 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import PrivateRoute from "./components/PrivateRoute";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="min-h-screen overflow-hidden block relative pt-[70px] sm:pb-80 pb-[650px]">
       <Router>
         <Header />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<PrivateRoute />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route
-            path="/category/:categoryName/:listingId"
-            element={<Listing />}
-          />
-          <Route path="/offers" element={<Offers />} />
-          <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="/create-listing" element={<PrivateRoute />}>
-            <Route path="/create-listing" element={<CreateListing />} />
-          </Route>
-          <Route path="/edit-listing" element={<PrivateRoute />}>
-            <Route path="/edit-listing/:listingId" element={<EditListing />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+        <ScrollToTop>
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/profile" element={<PrivateRoute />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+            <Route
+              path="/category/:categoryName/:listingId"
+              element={<Listing />}
+            />
+            <Route path="/offers" element={<Offers />} />
+            <Route path="/category/:categoryName" element={<Category />} />
+            <Route path="/create-listing" element={<PrivateRoute />}>
+              <Route path="/create-listing" element={<CreateListing />} />
+            </Route>
+            <Route path="/edit-listing" element={<PrivateRoute />}>
+              <Route
+                path="/edit-listing/:listingId"
+                element={<EditListing />}
+              />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
       </Router>
       <ToastContainer
         position="bottom-right"
